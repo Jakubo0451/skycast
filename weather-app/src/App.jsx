f614367e0f273a9f6896481cd8e5d2a065fd914e
Jakubo0451 <jakubo@stud.ntnu.no>
2026-07-02 19:45:25 +0200

    fix: fixed broken url

--- weather-app/src/App.jsx
+++ weather-app/src/App.jsx
@@ -18,36 +18,37 @@ function App() {
     try {
       const geocodeUrl = `https://geocoding-api.open-meteo.com/v1/search?name=${encodeURIComponent(city.trim())}&count=1&language=en`;
       console.log(geocodeUrl);
-
       const geoResponse = await fetch(geocodeUrl);
+      
       if (!geoResponse.ok) {
         throw new Error("Network response was not ok");
       }
       const geoData = await geoResponse.json();
-
+      
       if (!geoData.results || geoData.results.length === 0) {
         throw new Error("City not found");
       }
-
+      
       const { latitude, longitude, name, country } = geoData.results[0];
       console.log(`Found: ${name}, ${country}, (${latitude}, ${longitude})`);
-
-      const weatherUrl = `https://open-meteo.com${latitude}&longitude=${longitude}&current=temperature_2m`;
-
+      const weatherUrl = `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current=temperature_2m`;
       const weatherResponse = await fetch(weatherUrl);
-
+      
       if (!weatherResponse.ok) {
         throw new Error("Weather service error");
       }
-
+      
       const weatherData = await weatherResponse.json();
-
       console.log("Weather data: ", weatherData);
       setWeather(weatherData);
+
     } catch (error) {
         console.error("Error caught in block: ", error);
         setError(error.message || "Something went wrong");
         setWeather(null)
+    
+    } finally {
+        setLoading(false)
     }
   };
 
