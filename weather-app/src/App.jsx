44b8836608ca8f124d1e227c592e07520e8e594d
Jakubo0451 <jakubo@stud.ntnu.no>
2026-07-03 20:11:33 +0200

    fix: split the logic into a service file

--- weather-app/src/App.jsx
+++ weather-app/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import "./App.css";
 import WeatherCard from "./components/WeatherCard";
+import fetchWeather from "./services/weatherService.js"
 
 function App() {
   const [city, setCity] = useState("");
@@ -17,39 +18,7 @@ function App() {
     setLoading(true);
 
     try {
-      const geocodeUrl = `https://geocoding-api.open-meteo.com/v1/search?name=${encodeURIComponent(city.trim())}&count=1&language=en`;
-      console.log(geocodeUrl);
-      const geoResponse = await fetch(geocodeUrl);
-
-      if (!geoResponse.ok) {
-        throw new Error("Network response was not ok");
-      }
-      const geoData = await geoResponse.json();
-
-      if (!geoData.results || geoData.results.length === 0) {
-        throw new Error("City not found");
-      }
-
-      const { latitude, longitude, name, country } = geoData.results[0];
-      console.log(`Found: ${name}, ${country}, (${latitude}, ${longitude})`);
-      const weatherUrl = `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current=temperature_2m,weather_code`;
-      const weatherResponse = await fetch(weatherUrl);
-
-      if (!weatherResponse.ok) {
-        throw new Error("Weather service error");
-      }
-
-      const weatherData = await weatherResponse.json();
-
-      const weatherInfo = {
-        city: name,
-        country: country,
-        temperature: weatherData.current.temperature_2m,
-        latitude: latitude,
-        longitude: longitude,
-        weatherCode: weatherData.current.weather_code,
-      };
-      console.log("Weather data: ", weatherInfo);
+      const weatherInfo = await fetchWeather(city)
       setWeather(weatherInfo);
     } catch (error) {
       console.error("Error caught in block: ", error);
@@ -59,6 +28,7 @@ function App() {
       setLoading(false);
     }
   };
+  
   return (
     <>
       <div>
