0ee61e88abff951d5396ed83ad739be076d0abfd
Jakubo0451 <jakubo@stud.ntnu.no>
2026-07-04 19:25:42 +0200

    fix: enlarged some fonts and added emojis

--- weather-app/src/components/WeatherCard.jsx
+++ weather-app/src/components/WeatherCard.jsx
@@ -1,16 +1,18 @@
 import { WEATHER_CODES } from "../utils/weatherCodes";
 
-function WeatherCard({weather}) {
+function WeatherCard({ weather }) {
   return (
     <div className="weather-card">
-      {weather && (
-        <>
-          <p>{weather.city}</p>
-          <p>{weather.country}</p>
-          <p>{weather.temperature}°C</p>
-          <p>{WEATHER_CODES[weather.weatherCode] || "Unknown weather"}</p>
-        </>
-      )}
+      <div className="location_container">
+        <p className="city_p">{weather.city} |</p>
+        <p className="country_p">{weather.country}</p>
+      </div>
+      <div className="weather_details">
+        <p className="temperature_p">{weather.temperature}°C |</p>
+        <p className="weather_p">
+          {WEATHER_CODES[weather.weatherCode] || "Unknown weather"}
+        </p>
+      </div>
     </div>
   );
 }
